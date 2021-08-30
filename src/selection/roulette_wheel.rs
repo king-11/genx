69330d1435ad5a38e33b68555f57b9c8d8e88d1d
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-08-30 20:28:50 +0530

    fix: sum of fitness value in roulette wheel
    was copied from rank hence had an extra 1f32 in it
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/selection/roulette_wheel.rs
+++ src/selection/roulette_wheel.rs
@@ -1,10 +1,7 @@
 use rand::{thread_rng, Rng};
 
 pub fn roulette_wheel_selection(fitness_values: &Vec<f32>, num_parents: usize) -> Vec<usize> {
-  let mut sum_of_fitness = 1f32;
-  for &val in fitness_values.iter() {
-      sum_of_fitness += val;
-  };
+  let sum_of_fitness = fitness_values.iter().sum::<f32>();
   let normalized_probabilities = fitness_values.iter().map(|&a| a/sum_of_fitness).collect::<Vec<f32>>();
 
   let mut selected_indices:Vec<usize> = Vec::new();
