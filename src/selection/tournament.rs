5947944f9ed9a1e0f9cb7ff5aa19e4021aba2ed6
Lakshya Singh <lakshay.singh1108@gmail.com>
2021-09-02 11:24:16 +0530

    fix: possible issue tournament selection
    tournament size greater than fitness len
    Signed-off-by: Lakshya Singh <lakshay.singh1108@gmail.com>

--- src/selection/tournament.rs
+++ src/selection/tournament.rs
@@ -1,5 +1,5 @@
 use rand::{seq::SliceRandom, rngs::StdRng, SeedableRng};
-use std::cmp::Ordering;
+use std::cmp::{Ordering, min};
 
 pub fn tournament_selection(fitness_values: &Vec<f32>, num_parents: usize, tournament_size: usize, seed: Option<u64>) -> Vec<usize> {
   let mut fitness_values_with_index : Vec<(f32, usize)> = Vec::new();
@@ -7,13 +7,14 @@ pub fn tournament_selection(fitness_values: &Vec<f32>, num_parents: usize, tourn
       fitness_values_with_index.push((value, i));
   };
 
+  let number_individuals = fitness_values.len();
   let mut selected_indices:Vec<usize> = Vec::new();
   for idx in 0..num_parents {
       let mut prng = match seed {
         Some(val) => StdRng::seed_from_u64(val*(idx as u64)),
         None => StdRng::from_entropy()
       };
-      let mut current_tournament = fitness_values_with_index.choose_multiple(&mut prng, tournament_size).cloned().collect::<Vec<(f32, usize)>>();
+      let mut current_tournament = fitness_values_with_index.choose_multiple(&mut prng, min(tournament_size, number_individuals)).cloned().collect::<Vec<(f32, usize)>>();
       current_tournament.sort_unstable_by(|a, b| b.partial_cmp(a).unwrap_or(Ordering::Equal));
       selected_indices.push(current_tournament[0].1);
   };
